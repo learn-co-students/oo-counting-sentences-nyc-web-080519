28ef3e3d2dd1e95709efc76410bff1d8a20ff262
Mason Balbera <mhbalbera@gmail.com>
2019-08-06 16:23:00 -0400

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,26 @@
-require 'pry'
+
 
 class String
 
   def sentence?
-    
+    self[-1] == "."
   end
 
   def question?
-
+    self[-1] == "?"
   end
 
   def exclamation?
-
+    self[-1] == "!"
   end
 
   def count_sentences
-
+    count = 0 
+    self.each_char.with_index do |char, i|
+      if (char == "." || char == "!" || char == "?") && !(self[i-1] == "." || self[i-1] == "!" || self[i-1] == "?")
+        count += 1 
+      end
+    end 
+    return count 
   end
 end
